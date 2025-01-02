c1650e9f3701173bd5c3f9b43272b70c4513803a
JEFFDARKO <139300293+JEFFDARKO@users.noreply.github.com>
2025-01-02 01:14:41 +0000

    fixing ci check

--- solutions/reverse_string.py
+++ solutions/reverse_string.py
@@ -1,34 +1,35 @@
 def reverse_string(string: str):
     """This function takes a string and returns the reverse form of the string
-    
+
     Parameters:
     string (str): The input to be reversed
-    
+
     Returns:
     str: The reversed version of the input string
-    
+
     Raises:
     AssertionError: If the input is not a string
-    
+
     Examples:
     >>> reverse_string('hello')
     'olleh'
-    
+
     >>> reverse_string('Python programming')
     'gnimmargorp nohtyP'
-    
+
     >>> reverse_string('12345')
     '54321'
-    
+
     >>> reverse_string('Madam')
     'madaM'
-    
+
     >>> reverse_string('racecar')
     'racecar'
     """
     # Check if input is a string
-    assert isinstance(string, str), 'Input must be a string'
-    
+    assert isinstance(string, str), "Input must be a string"
+
     return string[::-1]
 
-print(reverse_string('Evan Cole'))
+
+print(reverse_string("Evan Cole"))
