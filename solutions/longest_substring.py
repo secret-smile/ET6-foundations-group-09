5b300d3ec221d80a0431716f75f6df876089bdd2
Hiba-Daffallah <hibayassir21@gmail.com>
2025-01-06 01:04:13 +0000

    solving ruff

--- solutions/longest_substring.py
+++ solutions/longest_substring.py
@@ -7,7 +7,7 @@ Module contents:
     longest_substring: generates the longest substring without
     repetition for a given string.
 
-Created on 2025-01-01
+Created on 2025-01-05
 @author: Hiba Daffallah
 """
 
@@ -41,7 +41,7 @@ def longest_substring(input_string: str) -> str:
     assert isinstance(input_string, str)
 
     n = len(input_string)
-     # If the string is empty, return an empty string as the result
+    # If the string is empty, return an empty string as the result
     if n == 0:
         return ""
     # Initialize a set to keep track of unique characters in the current substring
@@ -51,18 +51,17 @@ def longest_substring(input_string: str) -> str:
     # Initialize the left pointer of the sliding window
     left = 0
     for right in range(n):
-        
         # If the character at the right pointer is already in the set,
         # slide the left pointer to remove characters until the duplicate is removed
         while input_string[right] in sub:
             sub.remove(input_string[left])
             left += 1
-            
-        # Add the current character at the right pointer to the set    
+
+        # Add the current character at the right pointer to the set
         sub.add(input_string[right])
         current_len = right - left + 1
-        
-         # If the current substring is longer than the previously recorded longest substring,
+
+        # If the current substring is longer than the previously recorded longest substring,
         # update the longest substring
         if current_len > len(longest_sub):
             longest_sub = input_string[left : right + 1]
