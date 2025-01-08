196c02a1060eb805a081c9785c9580fbdd27678f
yoolmalaak <yoolmalaak@gmail.com>
2025-01-08 17:26:14 +0000

    challenge/verify_data

---
+++ solution/tests/test_verify_data.py
@@ -0,0 +1,85 @@
+"""the unittests validate names,email address, password, credit card number
+    """
+import unittest
+
+import verify_data
+
+class TestValidation(unittest.TestCase):
+    
+    
+    def test_valid_name(self):
+        """test that the name pass the validation
+        """
+        self.assertTrue(verify_data.validate_name("Pope Francis"))
+        
+    def test_invalid_name(self):
+        """ test that the invalid name fail the validation
+        """
+        self.assertFalse(verify_data.validate_name("Mohameed1@234")) 
+        self.assertFalse(verify_data.validate_name("Huaaien195"))   
+        
+    def test_valid_id_number(self):
+        """test that the valid ID number pass the validation
+        """
+        self.assertTrue(verify_data.validate_id_number("204456247"))
+        
+    def test_invalid_id_number(self):
+        """test that the invalid ID number fail the validation
+        """
+        self.assertFalse(verify_data.validate_id_number("A25 3874 926"))
+        self.assertFalse(verify_data.validate_id_number("278 654 68"))   
+        self.assertFalse(verify_data.validate_id_number("2-76 901 254")) 
+            
+        
+    def test_valid_email(self):
+        """test that the email is valid and pass the check
+        """
+        self.assertTrue(verify_data.validate_email("yool@gmail.com"))
+        self.assertTrue(verify_data.validate_email("mahdia@school.edu"))
+        
+    
+    
+    def test_invalid_email(self):
+        """ test that invalid fail the validation
+        """
+        self.assertFalse(verify_data.validate_email("joseph-email")) 
+        self.assertFalse(verify_data.validate_email("@james.com"))
+        self.assertFalse(verify_data.validate_email("peter@gmail-com"))
+        self.assertFalse(verify_data.validate_email("ahmed@"))
+        
+        
+        
+    def test_valid_credit_card(self):
+        """ test the credit card number pass the Luhn algorithm
+        """
+        self.assertTrue(verify_data.validate_credit_card("6011 1111 1111 1117"))
+        self.assertTrue(verify_data.validate_credit_card("3782 8224 6310 005"))  
+        
+        
+    def test_invalid_credit_card(self):
+        """test the invalid credit card number fail the Luhn algorithm
+        """
+        self.assertFalse(verify_data.validate_credit_card("1234-5678 9875 0876"))
+        self.assertFalse(verify_data.validate_credit_card("123446acbdte")) 
+        self.assertFalse(verify_data.validate_credit_card("abcdefhgy")) 
+        
+        
+    def test_valid_password(self):
+        """ test that the valid password pass the validation criteria
+        """
+        self.assertTrue(verify_data.validate_password("Evans@2025!")) 
+        self.assertTrue(verify_data.validate_password("Abr@h@m233"))
+        self.assertTrue(verify_data.validate_password("Jer0megeo@")) 
+        
+        
+    def test_invalid_password(self):
+        """test that the invalid password fail the validation criteria
+        """
+        self.assertFalse(verify_data.validate_password("evans@2025!"))
+        self.assertFalse(verify_data.validate_password("Password.com"))
+        self.assertFalse(verify_data.validate_password("James_mit@"))
+        self.assertFalse(verify_data.validate_password("123456789"))
+        self.assertFalse(verify_data.validate_password("Job@1"))         
+        
+if __name__ == "__main__":
+    unittest.main()          
---
+++ solution/tests/test_verify_data_1.py
@@ -0,0 +1,146 @@
+"""
+test for verification function. the test validate the data that is passed the criteria, and vice versa.
+"""
+
+
+
+def validate_name(name):
+    """ validate the names to ensure that it contains alphabetical characters and spaces only
+
+    parameters:
+        name (str): the name to be validated 
+
+    Returns:
+        bool: true if the name is valid, false otherwise.
+    examples:
+    >>> validate_name("Camilla Massa")
+    True
+    >>> validate_name("Joe_Biden")
+    False
+    >>> validate_name("Mohammed122")
+    False
+    """
+    if all(char.isalpha() or char.isspace() for char in name):
+        return True
+    else:
+        return False
+
+def validate_id_number(id_number):
+    """ validate ID number that contains numbers only and has at least 9 digits
+
+    parameters:
+        ID_number (str): ID number to validate
+
+    Returns:
+        bool:  true if true, otherwise false
+    examples:
+    >>> validate_id_number("204456247")
+    True
+    >>> validate_id_number("204456")
+    False
+    >>> validate_id_number("2A4576983021765")
+    False
+    >>> validate_id_number("24-6 7684 9085")
+    False
+    """ 
+    if  id_number.isdigit() and len(id_number) >= 9:
+        return True
+    else:
+        return False
+  
+    
+def validate_credit_card(card_number):
+    """ validate credit card number using the Luhn algorithm
+
+    parameters:
+        card_number (str): credit card number to be validated
+
+    Returns:
+        bool: true if the credit card number is valid, otherwise false
+    examples:
+    >>> validate_credit_card("6011 1111 1111 1117")
+    True
+    >>> validate_credit_card("3782 8224 6310 005")
+    True
+    >>> validate_credit_card("12345-6789038693")
+    False
+    >>> validate_credit_card("2A89497534975jc")
+    False
+    >>> validate_credit_card("abcdefghijkl")
+    False
+    """   
+    card_number = card_number.replace(" ", "")
+    if not card_number.isdigit():
+        return False
+    total = 0
+    reverse_digits = card_number[::-1]
+    for i, digit in enumerate(reverse_digits):
+        n = int(digit)
+        if i % 2 == 1:
+            n *= 2
+            if n > 9:
+                n -= 9
+        total += n
+    return total % 10 == 0
+    
+def validate_email(email):
+    """ Validate the email address that contain @ and dot(.)
+
+    parameters:
+        email (str): email address to be validated
+
+    Returns:
+        bool: true if email address is valid, false otherwise
+    examples:    
+    >>> validate_email("martha@gmail.com")
+    True
+    >>> validate_email("katrina@school.edu")
+    True
+    >>> validate_email("patrick-email.com")
+    False
+    >>> validate_email("adam@gmail_com")
+    False
+    >>> validate_email("ahmed@")
+    False
+    """   
+    if "@" not in email or "." not in email:
+        return False
+    at_index = email.index("@")
+    dot_index = email.rindex(".")
+    if at_index == 0 or dot_index < at_index + 2 or dot_index == len(email) - 1:
+        return False
+    return True
+def validate_password(password):
+    """ validate password base on the following criteria
+    - at least 8 characters
+    - at least one uppercase
+    - at least one lowercase
+    - at least one number
+    - at least one special character("!@#$%^&*()-+")
+
+    parameters:
+        password (str): password to be checked
+
+    Returns:
+        bool: true if password is valid, false otherwise.
+    examples:    
+    >>> validate_password("Johneavans@2026.com")
+    True
+    >>> validate_password("J@c0b.israel")
+    True
+    >>> validate_password("johnevans@2028.com")
+    False
+    >>> validate_password("Megan_mit$%")
+    False
+    >>> validate_password("202987423690")
+    False
+    >>> validate_password("Short2!")
+    False
+    """ 
+    if len(password) < 8:
+        return False
+    has_upper = any(char.isupper() for char in password)
+    has_lower = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+    has_special = any(char in "!@#$%^&*()-+" for char in password)
+    return has_upper and has_lower and has_digit and has_special    
